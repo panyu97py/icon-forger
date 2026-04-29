3210b0d33f2712f04c3271b7373b1a138274f424
yu.pan <panyupy@vip.qq.com>
2026-04-29 11:51:20 +0800

    feat: 替换组件名

--- packages/icon-forger-taro/src/base-icon.tsx
+++ packages/icon-forger-taro/src/base-icon.tsx
@@ -2,13 +2,13 @@ import { Text } from '@tarojs/components'
 import classNames from 'classnames'
 import React from 'react'
 
-export interface IconProps {
+export interface BaseIconProps {
     size?:number;
     color?:string;
     name?:string;
     className?:string;
 }
-export const Icon: React.FC<IconProps> = (props) => {
+export const BaseIcon: React.FC<BaseIconProps> = (props) => {
   const { color, size, name, className } = props
   return <Text className={classNames(`icon-${name}`, className)} style={{ color, fontSize: size || 16 }}></Text>
 }
